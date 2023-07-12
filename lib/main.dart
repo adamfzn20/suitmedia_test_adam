06ac6c8d29b2dec955f39a63f92c6ddb8ac66a4e
adamfzn20 <adam.fauzan.790.af@gmail.com>
2023-07-13 01:36:28 +0700

    fixing bug https

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:suitmedia_test_adam/my_app.dart';
+import 'package:suitmedia_test_adam/utils/http_overriding.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  HttpOverrides.global = HttpOverriding();
+  Provider.debugCheckInvalidValueType = null;
+
   runApp(const MyApp());
 }
